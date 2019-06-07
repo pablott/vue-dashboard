4dc9764f6a21962f95a0627d6e5ed486c1a66200
Pablo Trabajos <pablo.trabajos.tamayo@gmail.com>
2019-06-07 23:37:45 +0100

    Added vue component logic

--- js/component.js
+++ js/component.js
@@ -1,1 +1,66 @@
+var list = new Vue({
+  el: '#item_list',
+  data: {
+    //items: [{"name":"Adam"},{"name":"Emma"}], // initialize empty array
+    items: '', // initialize empty array
+    message: 'xxx',
+    test_array: [{
+        message: 'Foo'
+      },
+      {
+        message: 'Bar'
+      }
+    ]
+  },
 
+
+  mounted() { // when the Vue app is booted up, this is run automatically.
+    //var self = this // create a closure to access component in the callback below
+    //loadJSON(function(response) {
+    // Parse JSON string into object
+    //self.items = JSON.parse(response);
+    //self.items = [1,4,3];
+    //console.log(response);
+    //console.log(JSON.parse(response));
+    //console.log(self.items);
+    //});
+
+    //self.message = 'ggg';
+  }
+
+})
+
+
+//list.items = [1,4,3];
+//list.items = [{name:"Adam"},{name:"Emma"}]; // initialize empty array
+list.message = 'ggg';
+loadJSON(function(response) {
+  // Parse JSON string into object
+  list.items = JSON.parse(response);
+  //self.items = [1,4,3];
+  console.log(response);
+  console.log(JSON.parse(response));
+  console.log(list.items);
+});
+
+
+
+
+
+
+
+
+
+/*
+
+
+var example1 = new Vue({
+  el: '#item_list',
+  data: {
+    items: [
+      { message: 'Foo' },
+      { message: 'Bar' }
+    ]
+  }
+})
+*/

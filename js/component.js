79240219df159d9c5c2ad490402fa3a9569a083e
Pablo Trabajos <pablo.trabajos.tamayo@gmail.com>
2019-06-09 00:47:47 +0100

    Added sort by importance functionality

--- js/component.js
+++ js/component.js
@@ -2,11 +2,12 @@ var list = new Vue({
   el: '#alerts',
   data: {
     items: '', // initialize empty array
+    sorted: false, // soeted flag
   },
 
   methods: {
     // Method for unrolling alerts list
-    see_more: function (event) {
+    see_more: function(event) {
       // Check if tab was clicked
       if (event) {
         console.log('list $refs', this.$refs);
@@ -18,6 +19,33 @@ var list = new Vue({
         event.target.disabled = true;
         event.target.style.opacity = '.5';
       }
+    },
+    // Method for sorting list by importance
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#Description
+    sort: function(event) {
+      if (!this.sorted) {
+        // Sort by importance
+        this.items.sort(function(a, b) {
+          var importanceA = a.importance.toUpperCase(); // ignore upper and lowercase
+          var importanceB = b.importance.toUpperCase(); // ignore upper and lowercase
+          if (importanceA > importanceB) {
+            return -1;
+          }
+          if (importanceA < importanceB) {
+            return 1;
+          }
+
+          // importance must be equal
+          return 0;
+        });
+        this.sorted = true;
+        this.$refs.sorting.innerText = 'IMPORTANCE ▼';
+      } else {
+        // If it is sorted, reset the flag, change button text and reverse sorting
+        this.sorted = false;
+        this.$refs.sorting.innerText = 'IMPORTANCE ▲';
+        this.items.reverse();
+      }
     }
   },
 
@@ -26,7 +54,7 @@ var list = new Vue({
 
 })
 
-// Load JSON, parse, create objects
+// Load JSON with alerts items, parse, create objects
 loadJSON(function(response) {
   // Parse JSON string into items reactive data
   list.items = JSON.parse(response);

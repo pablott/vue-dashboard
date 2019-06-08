36593e97d655d767a8d6d75d92c90f018a191bf0
Pablo Trabajos <pablo.trabajos.tamayo@gmail.com>
2019-06-08 22:32:20 +0100

    Make the SEE MORE tab functional

--- js/component.js
+++ js/component.js
@@ -4,6 +4,17 @@ var list = new Vue({
     items: '', // initialize empty array
   },
 
+  methods: {
+    greet: function (event) {
+      // Pick
+      if (event) {
+        console.log('list $refs', this.$refs);
+        this.$refs.list.style.height = '100%'
+        //alert(event.target.tagName)
+      }
+    }
+  },
+
   mounted() { // when the Vue app is booted up, this is run automatically.
   }
 
@@ -13,7 +24,7 @@ var list = new Vue({
 loadJSON(function(response) {
   // Parse JSON string into items reactive data
   list.items = JSON.parse(response);
-  console.log(response);
-  console.log(JSON.parse(response));
-  console.log(list.items);
+  console.log('list response:', response);
+  console.log('list JSON.parse:', JSON.parse(response));
+  console.log('list Vue el items:', list.items);
 });

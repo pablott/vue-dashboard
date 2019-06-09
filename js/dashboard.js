ef1c51195ad1506e4d6576029bf2c4e25612ee3e
Pablo Trabajos <pablo.trabajos.tamayo@gmail.com>
2019-06-09 02:19:32 +0100

    Added basic dashbaord Vue instance and component

---
+++ js/dashboard.js
@@ -0,0 +1,33 @@
+// Define a new component called button-counter
+/*Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
+
+*/
+
+
+var dashboard = new Vue({ el: '#dashboard' })
+
+
+
+/*
+var dashboard = new Vue({
+  el: '#dashboard',
+  data: {
+
+  },
+
+  methods: {
+
+  },
+
+  mounted() { // when the Vue app is booted up, this is run automatically.
+  }
+
+})
+*/

20db26a05a567a06e8bee6da27f1fada26a9ec16
Pablo Trabajos <pablo.trabajos.tamayo@gmail.com>
2019-06-09 19:51:38 +0100

    Improve 'trend' component code and features (trend_text, values dispalyed)

--- js/components/trend/trend.js
+++ js/components/trend/trend.js
@@ -1,51 +1,64 @@
-// Define a new component called button-counter
-/*Vue.component('trend', {
-  data: function () {
-    return {
-      count: 0
-    }
-  },
-
-  props: {
-    value: String,
-  },
-
-  template: '{{value}}<button v-on:click="count++">You clicked me {{ count }} times.</button>'
-})
-*/
-
-
 Vue.component('trend', {
   data: function () {
     return {
-      count: 0
+      // data in a component is set inside a function to keep reactivity
     }
   },
 
   props: {
-    current: String,
+    // Take values from parent instance
     values: Array,
   },
 
   watch: {
+    // Watch the changes, this does nothing, just showing the feature and logging
     values: function(val) {
-      alert('values were changed');
+      console.log('values were changed to: ', val);
+    }
+  },
+
+  // It is discouraged mutating props of a component, so we have to use computed{} instead.
+  // This mutates values for the template without touching the prop 'values'.
+  // 'values' on parent instance can be mutated and it will propagate to children components.
+  computed: {
+    // Compute values from 'values'
+    get_current: function () {
+      var current = this.values[this.values.length-1];
+      return current;
+    },
+
+    get_diff: function () {
+      var current = this.values[this.values.length-1];
+      var previous = this.values[this.values.length-2];
+      var diff = current-previous;
+      return diff;
+    },
+
+    get_trend_text: function () {
+      var current = this.values[this.values.length-1];
+      var previous = this.values[this.values.length-2];
+
+      if (current>previous) {
+        trend_text = 'Increase';
+      } else {
+        trend_text = 'Decrease';
+      }
+      return trend_text;
     }
   },
 
   method: {
   },
 
-
-
-  //template: '<button v-on:click="count++">{{value}} You clicked me {{ count }} times.</button>'
+  // Template using string literals for splitting it in multilines
   template: `
     <div>
-      <div class="indicator"></div>
-      <div class="legend">
-        {{values}}
+      <div class="indicator">
+        {{get_current}}/{{get_diff}}
       </div>
-      <p ref="trend_text">INCREASE</p>
+      <div class="legend">
+        {{get_trend_text}}
+      </div>
     </div>
     `
 })

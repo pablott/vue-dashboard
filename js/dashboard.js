a98632a01279260ce99cdb1d3cc5fb75f98af78d
Pablo Trabajos <pablo.trabajos.tamayo@gmail.com>
2019-06-09 19:51:07 +0100

    Cleanup dashboard instance code

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,40 +1,11 @@
-// Define a new component called button-counter
-/*Vue.component('button-counter', {
-  data: function () {
-    return {
-      count: 0
-    }
-  },
-  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
-})
-
-*/
+// Define a new instance for the dashboard.
+// data set here can be accesed from every children component
+// (the displays for the stats)
 
 
 var dashboard = new Vue({
   el: '#dashboard',
   data: {
-    current: '104',
     values: [10, 20, 50, 70],
   },
-
 })
-
-
-
-/*
-var dashboard = new Vue({
-  el: '#dashboard',
-  data: {
-
-  },
-
-  methods: {
-
-  },
-
-  mounted() { // when the Vue app is booted up, this is run automatically.
-  }
-
-})
-*/

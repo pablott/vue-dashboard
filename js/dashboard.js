007ff51cb4b7da03b9a1e4a09c9214e3ceae4678
Pablo Trabajos <pablo.trabajos.tamayo@gmail.com>
2019-06-10 01:15:29 +0100

    Add functions to add, remove and randomize dashboard values

--- js/dashboard.js
+++ js/dashboard.js
@@ -17,7 +17,43 @@ var dashboard = new Vue({
       dashboard.$data.values = [10,10,50,25,45]
       dashboard.$data.values = [10,10,45]
       dashboard.$data.values = [10,10,50,25,100,32.7]
+      dashboard.add_value()
+      dashboard.remove_value()
+      dashboard.randomize()
     `);
-  }
+  },
 
+  methods: {
+    // Push random value to array
+    add_value: function () {
+      // Random number from 0 to 100, make sure is passed as int not string
+      var rand = parseInt(Math.random() * 100);
+      // Push number to values array
+      this.values.push(rand);
+    },
+
+    // Remove value from array
+    remove_value: function () {
+      if (this.values.length > 2) {
+        // Remove last element from array
+        this.values.pop();
+      } else {
+        console.log('Can\'t delete any more values');
+        return false;
+      }
+    },
+
+    // Randomize all values inside array
+    randomize: function () {
+      for (var i = 0; i < this.values.length; i++) {
+        // Random number from 0 to 100, make sure is passed as int not string
+        var rand = parseInt(Math.random() * 100);
+        this.values[i] = rand;
+        // Push so Vue can actually propagate the changes
+        // (just changing the value of an array item is not enough!)
+        this.values.push();
+      }
+    }
+
+  } // methods
 })
